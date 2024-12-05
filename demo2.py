5558063b05d6b80846a3193b7d317e5e11b827e7
watersalamander <118361497+watersalamander@users.noreply.github.com>
2024-12-05 11:14:16 +0700

    Create demo2.py

---
+++ demo2.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+st.set_page_config(page_title="Allocation",
+                   layout="centered")
+
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("https://moneyexpo.net/wp-content/uploads/2024/04/Merkel.jpg", use_column_width=True)
+
+
+gold = st.number_input('Percentage in Gold',
+                       min_value=0.0,
+                       max_value=100.0,
+                       value=20.0,
+                       step=1.0)
+bitcoin = st.number_input('Percentage in Bitcoin',
+                        min_value=0.0,
+                        max_value=100.0,
+                        value=20.0,
+                        step=1.0)
+reits = st.number_input('Percentage in REITs',
+                        min_value=0.0,
+                        max_value=100.0,
+                        value=20.0,
+                        step=1.0)
+stock = st.number_input('Percentage in Stock',
+                        min_value=0.0,
+                        max_value=100.0,
+                        value=20.0,
+                        step=1.0)
+bond = st.number_input('Percentage in Bond',
+                        min_value=0.0,
+                        max_value=100.0,
+                        value=20.0,
+                        step=1.0)
+
+data = {
+    'Asset': ['Gold', 'Bitcoin', 'REITs', 'Stock','Bond'],
+    'Values': [gold, bitcoin, reits, stock, bond]
+    }
+
+button = st.button('Calculate')
+
+if button:
+    total_return = ((gold*29.52)+(bitcoin*143.95)+(reits*12.34)+(stock*22.08)+(bond*4.45))*0.01
+    st.write("YTD Return: ", total_return, "%")
+
+    volatility = ((gold*15)+(bitcoin*72.9)+(reits*22.7)+(stock*19.6)+(bond*6.4))*0.01
+    st.write("1-Year Volatility: ", volatility, "%")
